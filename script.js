3d9927dd490493c27b292de10add9eb2cf8ff771
Gullied Mohamed <gulleidmohamed93@gmail.com>
2023-06-21 10:36:17 +0300

    largest number groups

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// Good Luck 💪🏾
+// // Good Luck 💪🏾
+
+function Largest(arr) {
+    const largenum = [];
+  
+    for (let i = 0; i < arr.length; i++) {
+      let largestNum = arr[i][0];
+  
+      for (let j = 1; j < arr[i].length; j++) {
+        if (arr[i][j] > largestNum) {
+          largestNum = arr[i][j];
+        }
+      }
+  
+      largenum.push(largestNum);
+    }
+  
+    return largenum;
+  }
+  
+  const numbers = [
+    [7, 3, 8, 11],
+    [54, 12, 43, 77],
+    [16, 11, 8]
+  ];
+  const largenum = Largest(numbers);
+  console.log(largenum);
